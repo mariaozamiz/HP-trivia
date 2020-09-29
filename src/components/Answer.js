08b35e494a49012983197244e90643b57401b619
Maria Ozamiz <ozamiz.m@gmail.com>
2020-09-29 10:32:23 +0200

    create Answer component

---
+++ src/components/Answer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Answer(props) {
+    return <button className="button__answer">{props.correctAnswer}</button>;
+}
+
+export default Answer;
--- src/components/AnswersList.js
+++ src/components/AnswersList.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import Answer from './Answer.js';
 
 function AnswersList(props) {
+    let incorrectAnswers = props.triviaCard.incorrectAnswers;
+    let correctAnswer = props.triviaCard.correctAnswer;
+
     return (
         <div className="main__answers">
-            <button className="button__answer">
-                {props.triviaCard.correctAnswer}
-            </button>
-            <button className="button__answer">
-                {props.triviaCard.incorrectAnswers[0]}
-            </button>
-            <button className="button__answer">
-                {props.triviaCard.incorrectAnswers[1]}
-            </button>
-            <button className="button__answer">
-                {props.triviaCard.incorrectAnswers[2]}
-            </button>
+            <Answer
+                incorrectAnswers={incorrectAnswers}
+                correctAnswer={correctAnswer}
+            />
         </div>
     );
 }
 
 export default AnswersList;
+
+// const charactersInfo = props.characters
+// .sort((a, b) => (a.name > b.name ? 1 : -1))
+// .map((character) => {
+//     return (
+//         <li key={character.id} className="character-card">
+//             <CharacterCard
+//                 id={character.id}
+//                 imageUrl={character.imageUrl}
+//                 name={character.name}
+//                 species={character.species}
+//             />
+//         </li>
+//     );
+// });
+// return <ul className="character-list">{charactersInfo}</ul>;
